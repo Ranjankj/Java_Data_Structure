c1c6178201f9810d66a24c9397a0bec6c1f437c6
Ranjan <63071934+Ranjankj@users.noreply.github.com>
2024-08-07 02:30:05 +0530

    Create Basic_code_Snippet.java

---
+++ Basic_code_Snippet.java
@@ -0,0 +1,7 @@
+Integer fmap[] = new Integer[26]; 
+Arrays.fill(fmap, 0); // fill the Integer Array with 0
+
+Arrays.sort(fmap,Collections.reverseOrder()); // sort Integer Array in reverse Order.
+
+ArrayList<Integer> list = new ArrayList<>(Arrays.asList(fmap)); // make a array list from array.
+

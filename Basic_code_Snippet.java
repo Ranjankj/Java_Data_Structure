d41e6774daba2b5efd22957045e241bba46f0269
Ranjan <63071934+Ranjankj@users.noreply.github.com>
2024-08-07 11:02:49 +0530

    Update Basic_code_Snippet.java

--- Basic_code_Snippet.java
+++ Basic_code_Snippet.java
@@ -3,5 +3,11 @@ Arrays.fill(fmap, 0); // fill the Integer Array with 0
 
 Arrays.sort(fmap,Collections.reverseOrder()); // sort Integer Array in reverse Order.
 
+// sorting using lambda expression it will work on the classes. 
+// not work for int work for Integer(wrapper class) 
+Arrays.sort(fmap,(a,b)->{
+      return a-b; // assending order 
+});
+
 ArrayList<Integer> list = new ArrayList<>(Arrays.asList(fmap)); // make a array list from array.
 
